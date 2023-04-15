16b00018422f0df3ca3018ab50324028cf874c87
arto cully <artocully@users.noreply.github.com>
2023-04-15 22:57:20 +0100

    add: download image with pwd

--- server.js
+++ server.js
@@ -17,11 +17,15 @@ app.use(express.urlencoded({ extended: true }));
 // set view engine to EJS
 app.use("view engine", "ejs");
 
+// ROUTES
 // set initial route to index
 app.get("/", (req, res) => {
   res.render("index");
 });
 
+app.route("file/:id").get(handleDownload).post(handleDownload);
+
+// METHODS
 // set POST upload file
 app.post("/upload", upload.single("file"), async (req, res) => {
   const fileData = {
@@ -41,3 +45,29 @@ app.post("/upload", upload.single("file"), async (req, res) => {
   // render fileLink to index view
   res.render("index", { fileLink: `${req.headers.origin}/file/${file.id}` });
 });
+
+async function handleDownload(req, res) {
+  const file = await File.findById(req.params.id);
+
+  if (file.password != null) {
+    if (req.body.password == null) {
+      res.render("password");
+      return;
+    }
+
+    if (!(await bcrypt.compare(req.body.password, file.password))) {
+      res.render("password", { error: true })
+      return;
+    }
+  }
+
+  file.downloadCount++
+
+  await file.save();
+
+  console.log('fileDownloadCount', file.downloadCount);
+
+  res.download(file.path, file.originalName);
+}
+
+app.listen(process.env.PORT);
